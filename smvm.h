b583ccc959972dd356c19882e27eae2784ea9d4b
Shreerang Vaidya <shreerangvaidya28@gmail.com>
2024-06-13 13:56:52 +0530

    more opcodes

--- smvm.h
+++ smvm.h
@@ -14,6 +14,7 @@
 typedef uint64_t u64;
 typedef uint16_t u16;
 typedef uint8_t u8;
+typedef int64_t i64;
 
 /*** util ***/
 
@@ -68,19 +69,30 @@ typedef enum smvm_opcode {
   op_halt = 0b00000,  // halts the program
   op_mov = 0b00001,   // mov x y
   op_add = 0b00010,   // add x y
-  op_sub = 0b00011,   // sub x y
-  op_jmp = 0b00100,   // jmp (to [d])
-  op_je = 0b00101,    // if (a == b) jmp (to [d])
-  op_jne = 0b00110,   // if (a != b) jmp (to [d])
-  op_jl = 0b00111,    // if (a < b) jmp (to [d])
-  op_loop = 0b01000,  // if (c > 0) dec c ; jmp (to [d])
-  op_inc = 0b01010,
-  op_dec = 0b01001,
+  op_addi = 0b00011,  // addi x y
+  op_addf = 0b00100,  // addf x y
+  op_sub = 0b00101,   // sub x y
+  op_subi = 0b00110,  // subi x y
+  op_subf = 0b00111,  // subf x y
+  op_mul = 0b01000,   // mul x y
+  op_muli = 0b01001,  // muli x y
+  op_mulf = 0b01010,  // mulf x y
+  op_div = 0b01011,   // div x y
+  op_divi = 0b01100,  // divi x y
+  op_divf = 0b01101,  // divf x y
+  op_jmp = 0b01110,   // jmp (to [d])
+  op_je = 0b01111,    // if (a == b) jmp (to [d])
+  op_jne = 0b10000,   // if (a != b) jmp (to [d])
+  op_jl = 0b10001,    // if (a < b) jmp (to [d])
+  op_loop = 0b10010,  // if (c > 0) dec c ; jmp (to [d])
+  op_inc = 0b10011,   // inc x
+  op_dec = 0b10100,   // dec x
   // NOTE: 1_____ > print instructions
-  // these might be temporary instructio#ns
-  op_print_int = 0b10000,
-  op_print_float = 0b10001,
-  op_print_str = 0b10010,  // prints till it encounters 0x0000
+  // these might be temporary instructions
+  op_print_int = 0b10101,
+  op_print_inti = 0b10110,
+  op_print_float = 0b10111,
+  op_print_str = 0b11000,  // prints till it encounters 0x0000
 } smvm_opcode;
 
 typedef enum smvm_register {
@@ -127,8 +139,12 @@ void smvm_free(smvm *vm);
 
 /* vm - helpers */
 u64 smvm_fetch_u64(smvm *vm);  // fetches 8 byte in native endian
+// fetches operands and performs operations
+static void smvm_handle_op(smvm *vm, u8 byte0,
+                           void (*op_func)(smvm *, u8, u8, u8));
 // converts to native endian from little-endian
 void *convert_endian(void *arr, int num);
+static void update_flags(smvm *vm, u64 result);
 
 /*** vm - inline helpers ***/
 
@@ -142,16 +158,12 @@ static inline u16 smvm_get_flag(smvm *vm, smvm_flag flag) {
   return vm->flags & flag;
 }
 
-// avoids code repetition
-static inline void smvm_handle_op(smvm *vm, u8 byte0,
-                                  void (*op_func)(smvm *, u8, u8, u8)) {
-  smvm_ip_inc(vm);
-  u8 byte1 = *smvm_fetch_inst_addr(vm);
-  u8 regx = (byte0 >> 5 & 0b100) | (byte1 >> 6 & 0b011);
-  u8 regy = (byte1 >> 3) & 0b111;
-  u8 regz = byte1 & 0b111;
-  op_func(vm, regx, regy, regz);
-  smvm_ip_inc(vm);
+static inline void smvm_set_flag(smvm *vm, smvm_flag flag) {
+  vm->flags |= flag;
+}
+
+static inline void smvm_reset_flag(smvm *vm, smvm_flag flag) {
+  vm->flags &= ~flag;
 }
 
 /*** vm - implementation ***/
@@ -210,17 +222,77 @@ void smvm_load_inst(smvm *vm, smvm_inst *inst, long num) {
 /* NOTE these aren't inline helpers! these are the real deal */
 static inline void add_op(smvm *vm, u8 regx, u8 regy, u8 regz) {
   vm->registers[regx] = vm->registers[regy] + vm->registers[regz];
+  update_flags(vm, vm->registers[regx]);
+}
+
+static inline void addi_op(smvm *vm, u8 regx, u8 regy, u8 regz) {
+  vm->registers[regx] = (i64)vm->registers[regy] + (i64)vm->registers[regz];
+  update_flags(vm, vm->registers[regx]);
+}
+
+static inline void addf_op(smvm *vm, u8 regx, u8 regy, u8 regz) {
+  double *dest = (double *)&vm->registers[regx];
+  double *src1 = (double *)&vm->registers[regy];
+  double *src2 = (double *)&vm->registers[regz];
+  *dest = *src1 + *src2;
 }
 
 static inline void sub_op(smvm *vm, u8 regx, u8 regy, u8 regz) {
   vm->registers[regx] = vm->registers[regy] - vm->registers[regz];
+  update_flags(vm, vm->registers[regx]);
 }
 
-static inline void mov_op(smvm *vm, u8 regx, u8 regy, u8 regz) {
-  vm->registers[regy] = vm->registers[regz];
+static inline void subi_op(smvm *vm, u8 regx, u8 regy, u8 regz) {
+  vm->registers[regx] = (i64)vm->registers[regy] - (i64)vm->registers[regz];
+  update_flags(vm, vm->registers[regx]);
 }
 
-// this is the *realer* deal (or not but who cares)
+static inline void subf_op(smvm *vm, u8 regx, u8 regy, u8 regz) {
+  double *dest = (double *)&vm->registers[regx];
+  double *src1 = (double *)&vm->registers[regy];
+  double *src2 = (double *)&vm->registers[regz];
+  *dest = *src1 - *src2;
+}
+
+static inline void mul_op(smvm *vm, u8 regx, u8 regy, u8 regz) {
+  vm->registers[regx] = vm->registers[regy] * vm->registers[regz];
+  update_flags(vm, vm->registers[regx]);
+}
+
+static void muli_op(smvm *vm, u8 regx, u8 regy, u8 regz) {
+  vm->registers[regx] = (i64)vm->registers[regy] * (i64)vm->registers[regz];
+  update_flags(vm, vm->registers[regx]);
+}
+
+static void mulf_op(smvm *vm, u8 regx, u8 regy, u8 regz) {
+  double *dest = (double *)&vm->registers[regx];
+  double *src1 = (double *)&vm->registers[regy];
+  double *src2 = (double *)&vm->registers[regz];
+  *dest = *src1 * *src2;
+}
+
+static void div_op(smvm *vm, u8 regx, u8 regy, u8 regz) {
+  vm->registers[regx] = vm->registers[regy] / vm->registers[regz];
+  update_flags(vm, vm->registers[regx]);
+}
+
+static void divi_op(smvm *vm, u8 regx, u8 regy, u8 regz) {
+  vm->registers[regx] = (i64)vm->registers[regy] / (i64)vm->registers[regz];
+  update_flags(vm, vm->registers[regx]);
+}
+
+static void divf_op(smvm *vm, u8 regx, u8 regy, u8 regz) {
+  double *dest = (double *)&vm->registers[regx];
+  double *src1 = (double *)&vm->registers[regy];
+  double *src2 = (double *)&vm->registers[regz];
+  *dest = *src1 / *src2;
+}
+
+static void mov_op(smvm *vm, u8 regx, u8 regy, u8 regz) {
+  vm->registers[regx] = vm->registers[regy];
+}
+
+// realer deal (maybe not)
 // TODO error return type
 void smvm_execute(smvm *vm) {
   while (true) {
@@ -237,8 +309,48 @@ void smvm_execute(smvm *vm) {
         smvm_handle_op(vm, byte0, add_op);
         break;
 
+      case op_addi:
+        smvm_handle_op(vm, byte0, addi_op);
+        break;
+
+      case op_addf:
+        smvm_handle_op(vm, byte0, addf_op);
+        break;
+
       case op_sub:
         smvm_handle_op(vm, byte0, sub_op);
+        break;
+
+      case op_subi:
+        smvm_handle_op(vm, byte0, subi_op);
+        break;
+
+      case op_subf:
+        smvm_handle_op(vm, byte0, subf_op);
+        break;
+
+      case op_mul:
+        smvm_handle_op(vm, byte0, mul_op);
+        break;
+
+      case op_muli:
+        smvm_handle_op(vm, byte0, muli_op);
+        break;
+
+      case op_mulf:
+        smvm_handle_op(vm, byte0, mulf_op);
+        break;
+
+      case op_div:
+        smvm_handle_op(vm, byte0, div_op);
+        break;
+
+      case op_divi:
+        smvm_handle_op(vm, byte0, divi_op);
+        break;
+
+      case op_divf:
+        smvm_handle_op(vm, byte0, divf_op);
         break;
 
       case op_mov:
@@ -271,13 +383,13 @@ void smvm_execute(smvm *vm) {
   }
 }
 
-/* vm helpers - implementation */
-
 void smvm_free(smvm *vm) {
+  listmv_free(&vm->memory);
   listmv_free(&vm->allocation);
   listmv_free(&vm->bytecode);
-  listmv_free(&vm->memory);
 }
+
+/* vm - helpers - implementation */
 
 u64 smvm_fetch_u64(smvm *vm) {
   u64 val;
@@ -288,6 +400,30 @@ u64 smvm_fetch_u64(smvm *vm) {
 
 // assuming little-endian, no-op
 void *convert_endian(void *arr, int num) { return arr; }
+
+static void smvm_handle_op(smvm *vm, u8 byte0,
+                           void (*op_func)(smvm *, u8, u8, u8)) {
+  smvm_ip_inc(vm);
+  u8 byte1 = *smvm_fetch_inst_addr(vm);
+  u8 regx = (byte0 >> 5) & 0b111;
+  u8 regy = (byte1 >> 3) & 0b111;
+  u8 regz = byte1 & 0b111;
+  smvm_ip_inc(vm);
+  op_func(vm, regx, regy, regz);
+}
+
+static void update_flags(smvm *vm, u64 result) {
+  if (result == 0) {
+    smvm_set_flag(vm, flag_z);
+  } else {
+    smvm_reset_flag(vm, flag_z);
+  }
+  if (result >> 63) {
+    smvm_set_flag(vm, flag_s);
+  } else {
+    smvm_reset_flag(vm, flag_s);
+  }
+}
 
 /*** util - implementation ***/
 
